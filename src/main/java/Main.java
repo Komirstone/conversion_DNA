45851d15f4ee26cb9311a49b7fbc0c8c44a5a18d
mirkorivas <mirkorivas@hotmail.com>
2024-02-18 19:34:31 +0100

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,20 @@ import static utils.Conversion.binaireToString;
 public class Main {
     public static void main(String[] args) {
         Conversion conversion = new Conversion();
+
         String seqADN = conversion.encoderMessageEnADN("Ceci est ma phrase a encoder.");
+        System.out.println("\n\n");
         String phrase = conversion.decoderADNEnMessage(seqADN);
+        System.out.println("\n\n");
+
 
         //Copie du fichier text.txt dans le dossier output
         String fichierEnSeqADN = conversion.encoderFichierEnADN("src/main/resources/fichiers/test.txt");
+        System.out.println("\n\n");
         String fichierDecode = conversion.decoderADNEnFichier(fichierEnSeqADN, "src/main/resources/output/");
+
+
+
+
     }
 }
